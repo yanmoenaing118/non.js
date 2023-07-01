976614fae19fd1309791165cd78a6e8b7d0ac43e
Yan Moe Naing <yanmoenaing@Yans-MacBook-Pro.local>
2023-07-01 09:31:10 +0630

    save

--- src/watchout/constants.ts
+++ src/watchout/constants.ts
@@ -1,8 +1,8 @@
 export const FPS = 60; // FramePerSecond
 export const DELTA_TIME = 1 / FPS;
 
-export const WIDTH = 480;
-export const HEIGHT = 320;
+export const WIDTH = 960;
+export const HEIGHT = 480;
 
 export const WORLD_WIDTH = WIDTH * 4;
 export const WORLD_HEIGHT = HEIGHT;
